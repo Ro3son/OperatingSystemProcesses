bb196e0dd6b2a88f164262f79d260cce07ba5096
robson_de_andrade <robsondeandrade23@gmail.com>
2025-03-17 14:59:19 -0400

    Created the Process class

---
+++ src/Process.java
@@ -0,0 +1,11 @@
+public class Process {
+    String pid;
+    String state;
+    String priority;
+
+    Process(String pid, String state, String priority) {
+        this.pid = pid;
+        this.state = state;
+        this.priority = priority;
+    }
+}
